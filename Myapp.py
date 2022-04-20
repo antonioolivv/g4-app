cf6dfab950a43da7417409b77a76edc8b2a2ef57
Pablo Monteoliva <53178907+pablo-monteoliva@users.noreply.github.com>
2022-04-20 21:06:22 +0200

    Update Myapp.py

--- Myapp.py
+++ Myapp.py
@@ -1,5 +1,6 @@
 import streamlit as st
 import pandas as pd
+import numpy as np
 st.title("My First Web App")
 st.image('./3AB54F9E-5771-4627-94FD-FAEA53432F73.webp')
 chart_data = pd.DataFrame(np.random.randn(20, 3), columns=['a', 'b', 'c'])
