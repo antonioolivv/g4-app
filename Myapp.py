a8b92d00d96141a7403ddc855487b9733177c8a6
Pablo Monteoliva <53178907+pablo-monteoliva@users.noreply.github.com>
2022-04-20 21:00:37 +0200

    Update Myapp.py
    Add photo link

--- Myapp.py
+++ Myapp.py
@@ -1,7 +1,7 @@
 import streamlit as st
 import pandas as pd
 st.title("My First Web App")
-st.image('./header.png')
+st.image('./3AB54F9E-5771-4627-94FD-FAEA53432F73.webp')
 chart_data = pd.DataFrame(np.random.randn(20, 3), columns=['a', 'b', 'c'])
 st.write(chart_data)
 option = st.selectbox('Options', ['a', 'b', 'c'])
