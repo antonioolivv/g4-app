82421238172d69296e7157db5db6b0701b94bd3f
Pablo Monteoliva <53178907+pablo-monteoliva@users.noreply.github.com>
2022-04-20 21:03:40 +0200

    Update Myapp.py

--- Myapp.py
+++ Myapp.py
@@ -3,6 +3,5 @@ import pandas as pd
 st.title("My First Web App")
 st.image('./3AB54F9E-5771-4627-94FD-FAEA53432F73.webp')
 chart_data = pd.DataFrame(np.random.randn(20, 3), columns=['a', 'b', 'c'])
-st.write(chart_data)
 option = st.selectbox('Options', ['a', 'b', 'c'])
 st.bar_chart(chart_data)
