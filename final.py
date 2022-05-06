75d6d7cc306f1767406a87afe11c56aa80424d25
antonioolivv <47591460+antonioolivv@users.noreply.github.com>
2022-05-06 21:19:47 +0200

    Update final.py

--- final.py
+++ final.py
@@ -4,8 +4,8 @@ import numpy as np
 import sqlite3 as sq
 
 
-st.title("My First Web App")
 st.image('3AB54F9E-5771-4627-94FD-FAEA53432F73.webp')
+st.title("Partner search tool")
 
 
 conn = sq.connect('ecsel_database.db')
